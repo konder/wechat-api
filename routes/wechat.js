f2cc5ae953dd5eeb2e4d288a8e494384bc9f166e
zhangnan <zhangnan@lngtop.com>
2015-01-14 19:12:28 +0800

    add more callback type

--- routes/wechat.js
+++ routes/wechat.js
@@ -6,22 +6,52 @@ var http = require('http');
 var wechat = require('wechat-enterprise');
 
 router.post('/', wechat(config, wechat.text(function (message, req, res, next) {
-    // TODO
-    config.reply.forEach(function (e) {
-        try {
-            var req = http.request(e, function () {
-            });
-            req.on('error', function (e) {
+        // TODO
+        config.reply.forEach(function (e) {
+            try {
+                var req = http.request(e, function () {
+                });
+                req.on('error', function (e) {
+                    console.log(e);
+                });
+                req.write(JSON.stringify(message));
+                req.end();
+            } catch (e) {
+                // TODO
                 console.log(e);
-            });
-            req.write(JSON.stringify(message));
-            req.end();
-        } catch (e) {
-            // TODO
-            console.log(e);
+            }
+        });
+        res.status(200).send('ok');
+    }).event(function (message, req, res, next) {
+        if (message.EventKey == 'shake'){
+            res.reply([
+                {
+                    title: '摇一摇',
+                    description: '参看大屏幕参加，切勿转发此链接给同事',
+                    picurl: 'http://nianhui.lngtop.com/img/shake.png',
+                    url: 'http://nianhui.lngtop.com/shake-wechat.html?userid=' + message.FromUserName
+                }
+            ]);
         }
-    });
-    res.status(200).send('ok');
-})));
+    }).image(function (message, req, res, next) {
+        console.log(message);
+        // TODO
+        config.reply.forEach(function (e) {
+            try {
+                var req = http.request(e, function () {
+                });
+                req.on('error', function (e) {
+                    console.log(e);
+                });
+                req.write(JSON.stringify(message));
+                req.end();
+            } catch (e) {
+                // TODO
+                console.log(e);
+            }
+        });
+        res.status(200).send('ok');
+    })
+));
 
 module.exports = router;
